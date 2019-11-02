e84f5459c5492b12017ca1bb7b87b0d0478801c0
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2019-11-01 23:07:00 -0600

    Updates

---
+++ precache-manifest.7a74f39a6bdae55ad153def17af2acd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5ec65855b6a7cfbac172cfed3809f3",
+    "url": "/RosaIsela31/Task-Fazt/index.html"
+  },
+  {
+    "revision": "1b5f2bc976f10a577bb4",
+    "url": "/RosaIsela31/Task-Fazt/static/css/main.8e95f36f.chunk.css"
+  },
+  {
+    "revision": "5fe2883e48d4712d611e",
+    "url": "/RosaIsela31/Task-Fazt/static/js/2.4803b0b2.chunk.js"
+  },
+  {
+    "revision": "1b5f2bc976f10a577bb4",
+    "url": "/RosaIsela31/Task-Fazt/static/js/main.4c3d7bc8.chunk.js"
+  },
+  {
+    "revision": "e6961e2355a6ed1795da",
+    "url": "/RosaIsela31/Task-Fazt/static/js/runtime-main.3b506c45.js"
+  }
+]);
